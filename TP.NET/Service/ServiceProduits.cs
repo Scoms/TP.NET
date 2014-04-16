82c1f405e79c979c461be9cf26d1f839f48737ea
Scoms <wieser.laurent@gmail.com>
2014-04-16 14:44:16 +0200

    SEND ALL THE BITCHES

--- TP.NET/Service/ServiceProduits.cs
+++ TP.NET/Service/ServiceProduits.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using TP.NETDAL.Model;
+using Utils;
 
 namespace TP.NET.Service
 {
     public class ServiceProduits
     {
-        public ServiceProduits()
+        private ErrorHandler _eh;
+        public ServiceProduits(ErrorHandler pEh)
         {
-
+            this._eh = pEh;
         }
 
         public List<Produits> findAll()
         {
             using (VenteEnLigneEntities context = new  VenteEnLigneEntities())
             {
-                return context.Produits.Include("Image").ToList();
+                return context.Produits.Include("Images").ToList();
             }
         }
     }
